cd9ae58b1a673ae68ddbb9a797da1eea3be71ae6
Koredake <koredake@163.com>
2021-05-23 21:33:13 +0800

    a

--- routes/admin.js
+++ routes/admin.js
@@ -2,40 +2,39 @@ var express = require('express');
 var router = express.Router();
 var student = require('./bean/student_info');
 var connection = require('../model/connect_mysql');
+const { json } = require('express');
 router.get('/admin',(req,res)=>{
-    let sql = 'select * from tab_student';
-    connection.query(sql,(err,results)=>{
-        res.render('admin',{messages:results})
+    let sql_check = 'select is_insert from tab_score';
+    connection.query(sql_check,(err,results_check)=>{
+        for(i in results_check){
+            if(results_check[i].is_insert =='0'){
+                let sql_show = 'select sname,school,class from tab_student st inner join tab_score sc on st.id = sc.student_id where sc.is_insert = 0';
+                connection.query(sql_show,(err,results_show)=>{
+                    res.render('admin',{messages:results_show})
+                })
+            }
+            else{
+                console.log(err);
+            }
+        }
     })
 })
 router.post('/admin',(req,res)=>{
-    let sql_check = 'select Chinese from tab_score'
-    connection.query(sql_check,(err,results_check)=>{
-        if(results_check != undefined){
-            
-        }
-    })
-    let sql_search = "select id from tab_student";
+    let sql_search = "select * from tab_score inner join tab_student on tab_score.student_id = tab_student.id where is_insert = 0";
     connection.query(sql_search,(err,sid_results)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            for(i in sid_results){
-                // console.log(req.body.chinese[i],req.body.math[i],req.body.english[i],req.body.composite[i],sid_results[i].id);
-                let sql_insert = "insert into tab_score(Chinese,math,English,composite,university,student_id) values('"+req.body.chinese[i]+"','"+req.body.math[i]+"','"+req.body.english[i]+"','"+req.body.composite[i]+"','"+req.body.university[i]+"','"+sid_results[i].id+"')";
-                connection.query(sql_insert,(err,insert_results)=>{
+        for(i in sid_results){
+                let sql_update = "update tab_score set Chinese = '"+req.body.chinese[i]+"',math = '"+req.body.math[i]+"',English = '"+req.body.english[i]+"',composite = '"+req.body.composite[i]+"',university = '"+req.body.university[i]+"',is_insert = '1' where student_id ="+sid_results[i].id+"";
+                connection.query(sql_update,(err,update_results)=>{
                     if(err){
                         console.log(err);
-                        res.redirect('admin',{code:200})
-                        return
                     }
                     else{
-                        res.redirect('admin',{code:201})
+                        res.send('保存成功')
                     }
                 })
             }
-        }
+            
+        
     })
 
         })
--- routes/student.js
+++ routes/student.js
@@ -3,19 +3,66 @@ var router = express.Router();
 // var student = require('./bean/student_info');
 var connection = require('../model/connect_mysql');
 router.get('/student',(req,res)=>{
-    let sql = 'select * from tab_student where sname ="'+req.session.now_name+'"'
-    connection.query(sql,(err,results,fields)=>{
-        for(i in results){
-            var uname = results[i].sname;
-            var school = results[i].school;
-            var b_class = results[i].class;
-        }
+    let sql_search = 'select * from tab_score inner join tab_student on tab_score.student_id = tab_student.id where tab_student.sname = "'+req.session.now_name+'"'
+    connection.query(sql_search,(err,search_results)=>{
+            if(search_results[0].is_insert == '1'&&req.session.now_name == search_results[0].sname){
+                console.log(search_results[0].is_insert);
+                let sql = 'select * from tab_student where sname ="'+req.session.now_name+'"'
+                connection.query(sql,(err,results,fields)=>{
+                        var uname = results[0].sname;
+                        var school = results[0].school;
+                        var b_class = results[0].class;
+                    
         req.session.school=school;
         req.session.b_class=b_class;
-        // console.log(results);
-        // console.log(uname,school,b_class);
-        res.render("student",{user:uname,b_school:school,be_class:b_class})
+                    var chinese = search_results[0].Chinese;
+                    var math = search_results[0].math;
+                    var english = search_results[0].English;
+                    var composite = search_results[0].composite;
+                    var university = search_results[0].university;
+                    var all = chinese+math+english+composite;
+                    // console.log(results);
+                    // console.log(uname,school,b_class);
+                    res.render("grmp",{user:uname,b_school:school,be_class:b_class,Chinese:chinese,Math:math,English:english,Composite:composite,All:all,University:university})
+                })
+            }
+            else if(search_results[0].is_insert == '0'&&req.session.now_name == search_results[0].sname){
+                let sql = 'select * from tab_student where sname ="'+req.session.now_name+'"'
+                connection.query(sql,(err,results,fields)=>{
+                    for(i in results){
+                        var uname = results[0].sname;
+                        var school = results[0].school;
+                        var b_class = results[0].class;
+                    }
+        req.session.school=school;
+        req.session.b_class=b_class;
+                    var chinese = '-';
+                    var math = '-';
+                    var english = '-';
+                    var composite = '-';
+                    var university = '-';
+                    var all = '-';
+                    // console.log(results);
+                    // console.log(uname,school,b_class);
+                    res.render("grmp",{user:uname,b_school:school,be_class:b_class,Chinese:chinese,Math:math,English:english,Composite:composite,All:all,University:university})
+                })
+            }
+            else{
+                console.log(err);
+            }
+        
     })
+    // let sql = 'select * from tab_student where sname ="'+req.session.now_name+'"'
+    // connection.query(sql,(err,results,fields)=>{
+    //     for(i in results){
+    //         var uname = results[i].sname;
+    //         var school = results[i].school;
+    //         var b_class = results[i].class;
+    //     }
+    //     req.session.school=school;
+    //     req.session.b_class=b_class;
+    //     res.render("student",{user:uname,b_school:school,be_class:b_class})
+    // })
 
 
     // console.log(uname,school,b_class);
